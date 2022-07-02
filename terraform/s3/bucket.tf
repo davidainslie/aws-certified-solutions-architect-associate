0f6a2e3b06e43f3be11c645b7480bcc107f48fa6
David Ainslie <dainslie@gmail.com>
2022-07-02 21:27:43 +0100

    S3 website

--- terraform/s3/bucket.tf
+++ terraform/s3/bucket.tf
@@ -1,6 +1,5 @@
-resource "aws_s3_bucket" "s3-bucket" {
+resource "aws_s3_bucket" "s3-website-bucket" {
   bucket_prefix = "my-bucket-"
-
   force_destroy = true
 
   tags = {
@@ -8,24 +7,69 @@ resource "aws_s3_bucket" "s3-bucket" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "s3-bucket-access" {
-  bucket = aws_s3_bucket.s3-bucket.id
+resource "aws_s3_bucket_public_access_block" "s3-website-bucket-access" {
+  bucket = aws_s3_bucket.s3-website-bucket.id
 
-  # Block public access
-  block_public_acls   = true
-  block_public_policy = true
-  ignore_public_acls = true
-  restrict_public_buckets = true
+  # Static website that has public access
+  block_public_acls   = false
+  block_public_policy = false
+  ignore_public_acls = false
+  restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_versioning" "s3-bucket-versioning" {
-  bucket = aws_s3_bucket.s3-bucket.id
+resource "aws_s3_bucket_versioning" "s3-website-bucket-versioning" {
+  bucket = aws_s3_bucket.s3-website-bucket.id
 
   versioning_configuration {
     status = "Enabled"
   }
 }
 
-output "terraform-state-aws-s3-bucket" {
-  value = aws_s3_bucket.s3-bucket.bucket
+resource "aws_s3_bucket_policy" "s3-website-bucket-policy" {
+  bucket = aws_s3_bucket.s3-website-bucket.id
+
+  policy = <<-EOT
+    {
+      "Version": "2012-10-17",
+      "Statement": [{
+        "Sid": "PublicReadGetObject",
+        "Effect": "Allow",
+        "Principal": "*",
+        "Action": [
+          "s3:GetObject"
+        ],
+        "Resource": [
+          "arn:aws:s3:::${aws_s3_bucket.s3-website-bucket.bucket}/*"
+        ]
+      }]
+    }
+  EOT
+}
+
+resource "aws_s3_bucket_website_configuration" "s3-website-configuration" {
+  bucket = aws_s3_bucket.s3-website-bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_object" "s3-bucket-website-files" {
+  for_each = fileset(".", "*.html")
+  bucket = aws_s3_bucket.s3-website-bucket.bucket
+  key = each.value
+  source = "./${each.value}"
+  content_type = "text/html"
+}
+
+output "s3-website-bucket" {
+  value = aws_s3_bucket.s3-website-bucket.bucket
+}
+
+output "s3-website-endpoint" {
+  value = aws_s3_bucket_website_configuration.s3-website-configuration.website_endpoint
 }
