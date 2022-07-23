efd6fe13e3a2b20a8b9070c00200ae5378c45406
David Ainslie <dainslie@gmail.com>
2022-07-23 11:17:13 +0100

    Improve terraform

--- terraform/ec2/roles/ec2.tf
+++ terraform/ec2/roles/ec2.tf
@@ -20,61 +20,6 @@ resource "local_sensitive_file" "pem-file" {
   content = tls_private_key.private-key.private_key_pem
 }
 
-# Create VPC in us-east-1
-resource "aws_vpc" "vpc" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "my-vpc"
-  }
-}
-
-# Create subnet # 1 in us-east-1
-resource "aws_subnet" "subnet" {
-  availability_zone = element(data.aws_availability_zones.azs.names, 0)
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = "10.0.1.0/24"
-}
-
-# Create IGW in us-east-1
-resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.vpc.id
-}
-
-# Get main route table to modify
-data "aws_route_table" "main-route-table" {
-  filter {
-    name   = "association.main"
-    values = ["true"]
-  }
-
-  filter {
-    name   = "vpc-id"
-    values = [aws_vpc.vpc.id]
-  }
-}
-
-# Create route table in us-east-1
-resource "aws_default_route_table" "internet-route" {
-  default_route_table_id = data.aws_route_table.main-route-table.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "my-route-table"
-  }
-}
-
-# Get all available AZ's in VPC for master region
-data "aws_availability_zones" "azs" {
-  state = "available"
-}
-
 # Create SG for allowing TCP/80 & TCP/22
 resource "aws_security_group" "sg" {
   name        = "sg"
---
+++ terraform/ec2/roles/vpc.tf
@@ -0,0 +1,41 @@
+# Get all available AZ's in VPC for master region
+data "aws_availability_zones" "azs" {
+  state = "available"
+}
+
+# Create VPC in us-east-1
+resource "aws_vpc" "vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "my-vpc"
+  }
+}
+
+# Create subnet # 1 in us-east-1
+resource "aws_subnet" "subnet" {
+  availability_zone = element(data.aws_availability_zones.azs.names, 0)
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = "10.0.1.0/24"
+}
+
+# Create IGW in us-east-1
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+# Create route table in us-east-1
+resource "aws_default_route_table" "internet-route" {
+  default_route_table_id = aws_vpc.vpc.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "my-route-table"
+  }
+}
